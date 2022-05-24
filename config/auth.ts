e0c22f2bd710fe02b6f3d272215d49335af07852
CHOIJEWON <alsrn6040@kakao.com>
2022-05-24 17:29:45 +0900

    config 폴더 삭제

--- config/auth.ts
+++
@@ -1,8 +0,0 @@
-const config = {
-    accessSecret: "bezkoder-access-secret-key",
-    refreshSecret: "bezkoder-refresh-secret-key",
-    jwtExpiration: 3600,        // 1 hour
-    jwtRefreshExpiration: 86400, // 24 hours
-}
-
-export default config;
